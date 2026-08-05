ad88de0d9c08c2e8b3d23ab60df75b07927fa1c5
Abubakarsiddiq Shah <shahabubakarsiddiq4@gmail.com>
2026-08-05 17:09:33 +0530

    Fix NotOwner error and clean up contract code

--- FundMe/Fundme.sol
+++ FundMe/Fundme.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe__NotOwner();
 
 contract FundMe {
 
@@ -57,7 +57,7 @@ contract FundMe {
     }
 
     modifier onlyOwner() {
-        if (msg.sender != i_owner) revert NotOwner();
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
 
